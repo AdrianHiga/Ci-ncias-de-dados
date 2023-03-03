4ca823da83dfd71b521103419ae2eb5549ba8163
AdrianHiga <102603609+AdrianHiga@users.noreply.github.com>
2023-03-02 21:36:21 -0300

    Aula01

---
+++ aula01.py
@@ -0,0 +1,38 @@
+# Aula 01
+
+# x = 'UNINOVE'
+# a = 10
+# b = 8.6
+# c = False
+
+# print(type(x))
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# x = 'UNINOVE'
+# y = 'ADRIAN'
+# a = 10
+# print(f'Bem vindo ao mundo de Python com {y} aluno da {x} que é, {a}!')
+
+# print('Dados do aluno')
+# nome = input('Digite seu nome: ')
+# idade = int(input('Informe sua idade: '))
+# print(f'{nome} tem, {idade} anos!!')
+
+
+x = 100
+y = 300
+z = 500
+w = 200
+
+k = x + y + z
+print(k)
+
+k = z + w / x
+print(k)
+print(z>w)
+print(w>z)
+print(y+w==z)
+print(z>w and w>y)
+print(z>w or w>y)
