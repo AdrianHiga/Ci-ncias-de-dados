11e0eb5a9d451d66dc99dd7f53a92c1052a3fed8
AdrianHiga <102603609+AdrianHiga@users.noreply.github.com>
2023-03-09 21:23:01 -0300

    Aula02

---
+++ aula02.py
@@ -0,0 +1,21 @@
+while True:
+    variavel = input("Digite um número: ")
+
+    if variavel == "1":
+        print(f"O valor digitado foi {variavel}, porém o valor impresso é 100")
+
+    elif variavel == "2":
+        print(f"O valor digitado foi {variavel}, porém o valor impresso é 200")
+
+    elif variavel == "3":
+        print(f"O valor digitado foi {variavel}, porém o valor impresso é 300")
+
+    elif variavel == "4":
+        print(f"O valor digitado foi {variavel}, porém o valor impresso é 400")
+
+    elif variavel == "5":
+        print(f"O valor digitado foi {variavel}, porém o valor impresso é 500")
+
+    else: 
+        print(f"O valor digitado foi {variavel}, porém o valor impresso é 1000")
+
