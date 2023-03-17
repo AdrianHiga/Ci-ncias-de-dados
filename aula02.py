14d506c284f29e0fe6d15290e6ad768f03c70dc5
AdrianHiga <102603609+AdrianHiga@users.noreply.github.com>
2023-03-17 10:37:59 -0300

    Aula03

--- aula02.py
+++ aula02.py
@@ -1,21 +1,28 @@
-while True:
+contador = 0
+while contador < 10:
     variavel = input("Digite um número: ")
-
     if variavel == "1":
         print(f"O valor digitado foi {variavel}, porém o valor impresso é 100")
-
+        contador +=1
+        continue
     elif variavel == "2":
         print(f"O valor digitado foi {variavel}, porém o valor impresso é 200")
-
+        contador +=1
+        continue
     elif variavel == "3":
         print(f"O valor digitado foi {variavel}, porém o valor impresso é 300")
-
+        contador +=1
+        continue
     elif variavel == "4":
         print(f"O valor digitado foi {variavel}, porém o valor impresso é 400")
-
+        contador +=1
+        continue
     elif variavel == "5":
         print(f"O valor digitado foi {variavel}, porém o valor impresso é 500")
-
+        contador +=1
+        continue
     else: 
         print(f"O valor digitado foi {variavel}, porém o valor impresso é 1000")
-
+        contador +=1
+        continue
+print(f"A quantidade de vezes que foi teclado um número foi: {contador} ")
---
+++ aula03.py
@@ -0,0 +1,8 @@
+contador = 0
+while (contador <5):
+    av1 = float(input("Informe av1 "))
+    av2 = float(input("Informe av2 "))
+    media = (av1+av2)/2
+    contador +=1
+    print("Sua média é igual a {}".format(media))
+print("Fim do programa")
